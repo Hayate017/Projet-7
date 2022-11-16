dd9e7982f8de8045116dc0e3358a4217568e1444
Paul-Valentin Blasco <paulvalentin.blasco@gmail.com>
2022-11-16 16:52:43 +0100

    Seconde partie de la réintégration des views

--- frontend/src/views/Error404.vue
+++ frontend/src/views/Error404.vue
@@ -1,11 +1,27 @@
 <template>
-    
+    <div>
+        <headerTop></headerTop>
+        <div class="error__bloc">
+            <h2 class="error__bloc__heading error__bloc__item">404</h2>
+            <img src="../assets/erreur404.png" alt=" Page introuvable !">
+            <p class="error__bloc__text error__bloc__item">En dépit d'une fouille méticuleuse nous n'avons pas trouvé la
+                page que vous recherchez !</p>
+        </div>
+        <footerBottom></footerBottom>
+    </div>
 </template>
 
 <script>
 
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
-
-
+export default {
+    name: 'Error404',
+    components: {
+        'headerTop': Header,
+        'footerBottom': Footer
+    },
+}
 
 </script>
