f2b1298001857d6ae309b489e464c821d5219893
Paul-Valentin Blasco <paulvalentin.blasco@gmail.com>
2022-11-16 16:48:08 +0100

    Remise en place progressive des views

--- frontend/src/views/Connection.vue
+++ frontend/src/views/Connection.vue
@@ -1,9 +1,22 @@
 <template>
-    
+    <div>
+        <img class="page__logo" src="../assets/icon-left-font-monochrome-black.png" alt="Logo de Groupomania">
+        <login> </login>
+        <footerBottom></footerBottom>
+    </div>
 </template>
 
 <script>
 
+import Login from '../components/Login'
+import Footer from '../components/Footer.vue'
 
+export default {
+    name: 'connection',
+    components: {
+        'login': Login,
+        'footerBottom': Footer
+    }
+}
 
 </script>
--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -1,9 +1,22 @@
 <template>
-    
+    <div>
+        <img class="page__logo" src="../assets/icon-left-font-monochrome-black.png" alt="Logo de Groupomania">
+        <signup> </signup>
+        <footerBottom> </footerBottom>
+    </div>
 </template>
 
 <script>
 
+import Signup from '../components/Signup'
+import Footer from '../components/Footer'
 
+export default {
+    name: 'landing',
+    components: {
+        'signup': Signup,
+        'footerBottom': Footer
+    }
+}
 
 </script>
